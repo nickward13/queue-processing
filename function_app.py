b6166231d268fb268c31098f487629e49140fd18
Nick Ward <nickward@microsoft.com>
2024-04-24 12:47:09 +1000

    save message body to output blob

--- function_app.py
+++ function_app.py
@@ -5,8 +5,10 @@ app = func.FunctionApp()
 
 @app.queue_trigger(arg_name="azqueue", queue_name="myqueue",
                                connection="6aec35_STORAGE") 
-@app.blob_output(arg_name="outblob", path="outcontainer/out.txt",
+@app.blob_output(arg_name="outblob", path="outcontainer/{rand-guid}.txt",
                                connection="6aec35_STORAGE")
 def queue_trigger(azqueue: func.QueueMessage, outblob: func.Out[str]):
+    body = azqueue.get_body().decode('utf-8')
     logging.info('Python Queue trigger processed a message: %s',
-                azqueue.get_body().decode('utf-8'))
+                body)
+    outblob.set(body)
