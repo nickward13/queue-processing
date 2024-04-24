b6b36b106ad29aba08a6777162d0005249cd1781
Nick Ward <nickward@microsoft.com>
2024-04-24 14:39:36 +1000

    automated message sending

---
+++ send-messages.py
@@ -0,0 +1,22 @@
+import os
+from azure.storage.queue import QueueServiceClient, QueueClient, BinaryBase64DecodePolicy, BinaryBase64EncodePolicy
+
+def send_message(queue_client):
+    message = '{"id":"1", "message": "something"}'
+    queue_client.send_message(bytes(message, 'utf-8'))
+    print("Message sent")
+
+# get connection_string from environment variable
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+# queue_service = QueueServiceClient.from_connection_string(conn_str=connection_string)
+# queue_client = queue_service.get_queue_client("myqueue")
+
+base64_queue_client = QueueClient.from_connection_string(
+    conn_str=connection_string,
+    queue_name="myqueue",
+    message_encode_policy=BinaryBase64EncodePolicy(),
+    message_decode_policy=BinaryBase64DecodePolicy())
+
+for i in range(50):
+    send_message(base64_queue_client)
